8626a60b6d885c33b3a32afaaec3ee774ee27a0a
Lena Petelina <le@htmlacademy.ru>
2019-03-05 19:05:22 +0300

    шаблонизирует

---
+++ src/get-task.js
@@ -0,0 +1,34 @@
+export default () => ({
+  title: [
+    `Изучить теорию`,
+    `Сделать домашку`,
+    `Пройти интенсив на соточку`,
+  ][Math.floor(Math.random() * 3)],
+  dueDate: Date.now() + 1 + Math.floor(Math.random() * 7) * 24 * 60 * 60 * 1000,
+  tags: new Set([
+    `homework`,
+    `theory`,
+    `practice`,
+    `intensive`,
+    `keks`,
+  ]),
+  picture: `//picsum.photos/100/100?r=${Math.random()}`,
+  color: [
+    `black`,
+    `yellow`,
+    `blue`,
+    `green`,
+    `pink`,
+  ][Math.floor(Math.random() * 5)],
+  repeatingDays: {
+    'mo': true,
+    'tu': false,
+    'we': true,
+    'th': false,
+    'fr': false,
+    'sa': true,
+    'su': false,
+  },
+  isFavorite: true,
+  isDone: false
+});
